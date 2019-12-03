1b4c315589e07a0639204e2626be7f38ac601f81
Izaac de Souza Silva <izcss21@gmail.com>
2019-12-03 10:08:23 -0300

    Inicio Sistema de Reservas de Salas de Reuniao.

---
+++ src/main/java/br/fatec/reusala/ReuSalaApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.reusala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReuSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/reusala/ReuSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.reusala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReuSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
